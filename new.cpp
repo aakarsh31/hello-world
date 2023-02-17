3a164ea8b0c3023230b01a1b32abd2bf6b5f092d
aakarsh31 <89195418+aakarsh31@users.noreply.github.com>
2023-02-17 16:53:59 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    
+    int i = 0;
+    int y = 10;
+
+    do{
+        cout << i << "\n";
+        i++;
+    }
+
+    while(i < 5);
+
+        
+     
+    return 0;
+}
+
